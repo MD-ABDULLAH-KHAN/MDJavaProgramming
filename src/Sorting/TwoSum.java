de9f5d195a13357f0c7c0e6df91397122bf44d48
MD-ABDULLAH-KHAN <khanak27085@gmail.com>
2025-09-03 19:44:56 +0530

    new commit

--- src/Sorting/TwoSum.java
+++ src/Sorting/TwoSum.java
@@ -1,7 +1,26 @@
 package Sorting;
+import java.util.Arrays;
 
 public class TwoSum {
     public static void main(String[] args) {
+        int[] arr = {7, 0, 4, 3, 2, 8, 10};
+        int target = 9;
 
+        Arrays.sort(arr);
+        int i = 0, j = arr.length - 1;
+
+        while (i < j) {
+            int sum = arr[i] + arr[j];
+            if (sum == target) {
+                System.out.println(arr[i] + " + " + arr[j] + " = " + target);
+                i++;
+                j--;
+            } else if (sum > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
     }
 }
+
