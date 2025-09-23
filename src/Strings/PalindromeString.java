f0a0bcff71413a0aca336465892fab946f73e143
MD-ABDULLAH-KHAN <khanak27085@gmail.com>
2025-09-23 21:18:30 +0530

    new commit

---
+++ src/Strings/PalindromeString.java
@@ -0,0 +1,28 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String : ");
+        String str = sc.nextLine().toLowerCase();
+
+        int i = 0, j = str.length() - 1;
+        boolean isPalindrome = true;
+
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                isPalindrome = false;
+                break;
+            }
+            i++;
+            j--;
+        }
+
+        if (isPalindrome)
+            System.out.println("A Palindrome");
+        else
+            System.out.println("Not A Palindrome");
+    }
+}
