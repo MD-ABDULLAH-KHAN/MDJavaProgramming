ac9a227d1fb309f80bbdb826825e8d6997248297
MD-ABDULLAH-KHAN <khanak27085@gmail.com>
2025-09-23 20:41:24 +0530

    new commit

---
+++ src/Strings/countVowels.java
@@ -0,0 +1,17 @@
+package Strings;
+import java.util.Scanner;
+public class countVowels {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String : ");
+        String str1 = sc.nextLine();
+        String str = str1.toLowerCase();
+        int n = str.length();
+        int count=0;
+        for(int i =0 ;i<n;i++){
+            char ch = str.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') count++;
+        }
+        System.out.println(count);
+    }
+}
