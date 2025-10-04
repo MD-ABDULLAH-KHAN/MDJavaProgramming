1326ad2b78ceff74099040f6eb06bab1234ad462
MD-ABDULLAH-KHAN <khanak27085@gmail.com>
2025-10-04 11:19:36 +0530

    new commit

--- src/MultiDimensionalArrays/RowWIthMaxSum.java
+++ src/MultiDimensionalArrays/RowWIthMaxSum.java
@@ -2,19 +2,32 @@ package MultiDimensionalArrays;
 
 public class RowWIthMaxSum {
     public static void main(String[] args) {
-        int[][] arr ={{6,0,2,7,6},{1,3,7,2,2},{9,9,4,5,2}};
-        int row=-1;
-        int maxSum=Integer.MIN_VALUE;
+//        int[][] arr ={{6,0,2,7,6},{1,3,7,2,2},{9,9,4,5,2}};
+//        int row=-1;
+//        int maxSum=Integer.MIN_VALUE;
+//        for(int i=0;i<arr.length;i++){
+//            int rowSum=0;
+//            for(int j=0;j<arr[0].length;j++){
+//                rowSum+=arr[i][j];
+//            }
+//            if(rowSum>maxSum){
+//                maxSum=rowSum;
+//                row=i;
+//            }
+//        }
+//        System.out.println(row+" "+maxSum);
+//    }
+        int[][] arr ={{2,8,3,4,7},{7,2,1,6,3},{5,5,4,1,4},{3,1,8,2,6}};
+        int min=Integer.MAX_VALUE;
         for(int i=0;i<arr.length;i++){
-            int rowSum=0;
+            int max=Integer.MIN_VALUE;
             for(int j=0;j<arr[0].length;j++){
-                rowSum+=arr[i][j];
+                if(arr[i][j]>max) max=arr[i][j];
             }
-            if(rowSum>maxSum){
-                maxSum=rowSum;
-                row=i;
+            if(max<min){
+                min=max;
             }
         }
-        System.out.println(row+" "+maxSum);
+        System.out.println(min);
     }
 }
---
+++ src/Recursion/GCD.java
@@ -0,0 +1,21 @@
+package Recursion;
+import java.util.*;
+public class GCD {
+    public  static int recursiveGCD(int a,int b){
+        if(b%a==0) return a;
+        return recursiveGCD(b%a,a);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Two Numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+//        int n = Math.min(a,b);
+//        int gcd=0;
+//        for(int i=1;i<=n;i++){
+//            if(a%i==0 && b%i==0)
+//                gcd=i;
+//        }
+        System.out.println(recursiveGCD(a,b));
+    }
+}
